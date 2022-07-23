513b074bca4bb3832746c8672ca3bac4c9a7a8f7
Mitchell Miller <mitchell.miller@arcurve.com>
2022-07-23 08:58:58 -0600

    Added React Router
    - Router go brrrr

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Application } from './pages/Application';
+import { Landing } from './pages/Landing'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='main'> {/* Apply global styles with the main class */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/app" element={<Application />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 )
---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,8 @@
+export const Landing = () => {
+
+    return(
+        <div>
+            // Landing Page
+        </div>
+    )
+}
