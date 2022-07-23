48bfab90b6cb784f065a59694b22e43d0c19a982
Taylor Morgan <100230329+TaylorJMorgan@users.noreply.github.com>
2022-07-23 09:24:48 -0600

    Small changes - merge with dev

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -4,10 +4,10 @@ import Nav from 'react-bootstrap/Nav'
 function Navigation() {
     return (
         <Navbar bg="dark" variant="dark" expand="md">
-            <Navbar.Brand className="brand-name navbar-brand ms-3" href="#home">Attractech</Navbar.Brand>
+            <Navbar.Brand className=" fs-3 brand-name navbar-brand ms-3" href="#home">Attractech</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
-                <Nav className="ms-auto me-3">
+                <Nav className="fs-5 ms-auto me-3">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#app">App</Nav.Link>
                     <Nav.Link href="#contact">Contact</Nav.Link>
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,14 +2,14 @@ import Navigation from '../components/Navigation'
 
 export const Landing = () => {
 
-    return(
-        <div>
+    return (
+        <div className="conatiner py-4">
             <Navigation />
-            <div className="conatiner py-4">
+            <div className="container py-4">
                 <div className="p-5 mb-4 bg-light rounded-3">
-                    <h1 className="display-5 fw-bold">Explore Calgary like never before</h1>
-                    <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <button className="btn btn-dark btn-lg" type="button">Let's Start</button>
+                    <h2 className="fw-bold">Explore Calgary like never before</h2>
+                    <p className="col-md-8 fs-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <button className="btn btn-primary btn-lg" type="button">Let's Start</button>
                 </div>
             </div>
         </div>
