e4841ee7a47fa1229fb5c0ce9aae5bc8e6accc6c
Mitchell Miller <mitchell.miller@arcurve.com>
2022-07-23 13:21:22 -0600

    Added Questions. Needs styling.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -8,9 +8,9 @@ function Navigation() {
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav className="ms-auto me-3">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#app">App</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/app">App</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
