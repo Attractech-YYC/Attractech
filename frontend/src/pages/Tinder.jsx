031efca991bb2ac4f3c6194cc3c73c5d37f7d1b2
Mitchell Miller <mitchell.miller@arcurve.com>
2022-07-24 09:19:02 -0600

    Auto stash before merge of "Taylor" and "origin/Taylor"

--- frontend/src/pages/Tinder.jsx
+++ frontend/src/pages/Tinder.jsx
@@ -11,16 +11,17 @@ import { Footer } from "../components/Footer";
 //I called this Tinder because it has cards that you can swipe left or right, just like Tinder.
 export const Tinder = ({ activity, filterData, movePage, saveData, list }) => {
 
-    const [data,setData] = useState(undefined);
+    const [data, setData] = useState(undefined);
 
     useEffect(() => {
         const getData = async () => {
             let res = await axios.get("http://3.96.135.171:8088/api/activity/filter?prefrences=" + activity)
             //TODO: Pass the activity and filterData to the API and filter the results
+            console.log(res.data)
             setData(res.data)
         }
         getData()
-    },[])
+    }, [])
 
 
     return (
@@ -29,22 +30,22 @@ export const Tinder = ({ activity, filterData, movePage, saveData, list }) => {
             <div className="row justify-content-center text-center vh-100">
                 <div className="col-8">
                     {
-                        array == undefined && <div className="p-5 bg-light rounded-3 footer-space"><h1 className="fw-bold fs-1">Loading...</h1></div>
+                        data == undefined && <div className="p-5 bg-light rounded-3 footer-space"><h1 className="fw-bold fs-1">Loading...</h1></div>
                     }
-            {
-                data != undefined &&
-                <div className="container py-4">
-                    {data.map((element, index) => {
-                        return <SwipeCard
-                            key={index}
-                            setPref={saveData}
-                            name={element.name}
-                            desc={element.description}
-                            cost={element.costs}
-                            time={element.time_commitment}
-                            index={index} />
-                            
-                    })}
+                    {
+                        data != undefined &&
+                        <div className="container py-4">
+                            {data.map((element, index) => {
+                                return <SwipeCard
+                                    key={index}
+                                    setPref={saveData}
+                                    name={element.name}
+                                    desc={element.description}
+                                    cost={element.costs}
+                                    time={element.time_commitment}
+                                    index={index} />
+
+                            })}
                         </div>
                     }
 
