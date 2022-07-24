a169559e937c27076db38ba3ac877422ccc29e2f
heimonsy <heimonsy@gmail.com>
2022-07-24 09:49:31 -0600

    fixes

--- cms/src/component/Header.js
+++ cms/src/component/Header.js
@@ -10,7 +10,7 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/activities">Cale</Navbar.Brand>
+        <Navbar.Brand href="/activities">Attractech</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
